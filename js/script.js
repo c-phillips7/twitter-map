84c99c867905e3af7b9afdb6f5d2d3b0b4978135
Sihayah <sihaya_a@hotmail.com>
2021-11-15 15:34:20 -0500

    test data + style

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-// need real url to fetch to get check validity
-fetch("https://api.twitter.com/1.1/trends/newyork.json?id=1").then(function(response){
-    if(response.ok){
-        response.json().then(function(myData){
-            generateTrends(myData)
-        })
-    }
+var trendsDiv = document.querySelector(".trends-div")
+
+// lines 4-16 are just test data to see if func will run properly
+let testTrend = []
+testTrend.push({
+    name: "#atrend"
 })
+testTrend.push({
+    name: "#another-trend"
+})
+testTrend.push({
+    name: "#one-more-trend"
+})
+localStorage.setItem("trend", JSON.stringify(testTrend))
+var myTrend = JSON.parse(localStorage.getItem('trend'))
+console.log(myTrend)
+
 
 // function to generate li for each trend
-generateTrends = (data) => {
-    var trendsUl = $("<ul>").addClass("trends-ul")
-    $("trends-div").append(trendsUl)
-    // func to pull #s from data
-    for (var i = 0; i<data.trends; i++) {
-        var trendItem= $("<li>").addClass("trend-item").text(data.trends[i].name)
-        $(trendsUl).append(trendItem)
+generateTrendingList = (trend) => {
+    var trendsUl = document.createElement("ul")
+    trendsUl.classList= ("trends-ul")
+    trendsDiv.append(trendsUl)
+
+    //  pull #s from locally stored data
+    for (var i = 0; i<trend.length; i++) {
+        var trendItem= document.createElement("li")
+        trendItem.classList= ("trend-item slide-right")
+        trendItem.textContent = (trend[i].name)
+        trendsUl.append(trendItem)
     }
 }
+generateTrendingList(myTrend)
