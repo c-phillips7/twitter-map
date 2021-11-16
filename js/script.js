4985031acbbda7ea7c41d4e2da8650c23928d040
Sihayah <sihaya_a@hotmail.com>
2021-11-15 19:19:48 -0500

    commented out test data

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 var trendsDiv = document.querySelector(".trends-div")
 
 // lines 4-16 are just test data to see if func will run properly
-let testTrend = []
-testTrend.push({
-    name: "#atrend"
-})
-testTrend.push({
-    name: "#another-trend"
-})
-testTrend.push({
-    name: "#one-more-trend"
-})
-localStorage.setItem("trend", JSON.stringify(testTrend))
-var myTrend = JSON.parse(localStorage.getItem('trend'))
-console.log(myTrend)
+// let testTrend = []
+// testTrend.push({
+//     name: "#atrend"
+// })
+// testTrend.push({
+//     name: "#another-trend"
+// })
+// testTrend.push({
+//     name: "#one-more-trend"
+// })
+// localStorage.setItem("trend", JSON.stringify(testTrend))
+// var myTrend = JSON.parse(localStorage.getItem('trend'))
+// console.log(myTrend)
 
 
 // function to generate li for each trend
