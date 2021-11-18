5f851a873983bfc1edfed73fe21c540ad39efced
Brandon Diaz <brandonjustindiaz@yahoo.com>
2021-11-17 21:39:01 -0500

    fixed the list to represent states instead of cities made sure it is in order

--- js/script.js
+++ js/script.js
@@ -33,255 +33,251 @@ generateTrendingList = (trends) => {
         trendsUl.append(trendItem)
     }
 }
-
+// commented out cities because they are from the same state already listed
 var cityObjects = [{
-    name: "Albuquerque",
-    code: "2352824"
-},
-{
-    name: "Atlanta",
-    code: "2357024"
-},
-{
-    name: "Austin",
-    code: "2357536"
-},
-{
-    name: "Baltimore",
-    code: "22358820"
-},
-{
-    name: "Baton Rouge",
-    code: "2359991"
-},
-{
-    name: "Birmingham",
-    code: "2364559"
-},
-{
-    name: "Boston",
-    code: "2367105"
-},
-{
-    name: "Charlotte",
-    code: "2378426"
-},
-{
-    name: "Chicago",
-    code: "2379574"
-},
-{
-    name: "Cincinnati",
-    code: "2380358"
-},
-{
-    name: "Cleveland",
-    code: "2381475"
-},
-{
-    name: "Colorado Springs",
-    code: "2383489"
-},
-{
-    name: "Columbus",
-    code: "2383660"
-},
-{
-    name: "Dallas-Ft. Worth",
-    code: "2388929"
-},
-{
-    name: "Denver",
-    code: "2391279"
-},
-{
-    name: "Detroit",
-    code: "2391585"
-},
-{
-    name: "Fresno",
-    code: "2407517"
-},
-{
-    name: "Greensboro",
-    code: "2414469"
-},
-{
-    name: "Harrisburg",
-    code: "2418046"
-},
-{
-    name: "Honolulu",
-    code: "2423945"
-},
-{
-    name: "Houston",
-    code: "2424766"
-},
-{
-    name: "Indianapolis",
-    code: "2427032"
-},
-{
-    name: "Jackson",
-    code: "2428184"
-},
-{
-    name: "Jacksonville",
-    code: "2428344"
-},
-{
-    name: "Kansas City",
-    code: "2430683"
-},
-{
-    name: "Las Vegas",
-    code: "2436704"
-},
-{
-    name: "Long Beach",
-    code: "2441472"
-},
-{
-    name: "Los Angeles",
-    code: "2442047"
-},
-{
-    name: "Louisville",
-    code: "2442327"
-},
-{
-    name: "Memphis",
-    code: "2449323"
-},
-{
-    name: "Mesa",
-    code: "2449808"
-},
-{
-    name: "Miami",
-    code: "2450022"
-},
-{
-    name: "Milwaukee",
-    code: "2451822"
-},
-{
-    name: "Minneapolis",
-    code: "2452078"
-},
-{
-    name: "Nashville",
-    code: "2457170"
-},
-{
-    name: "New Haven",
-    code: "2458410"
-},
-{
-    name: "New Orleans",
-    code: "2458833"
-},
-{
-    name: "New York",
-    code: "2459115"
-},
-{
-    name: "Norfolk",
-    code: "2460389"
-},
-{
-    name: "Oklahoma City",
-    code: "2464592"
-},
-{
-    name: "Omaha",
-    code: "2465512"
-},
-{
-    name: "Orlando",
-    code: "2466256"
-},
-{
-    name: "Philadelphia",
-    code: "2471217"
-},
-{
-    name: "Phoenix",
-    code: "2471390"
-},
-{
-    name: "Pittsburgh",
-    code: "2473224"
-},
-{
-    name: "Portland",
-    code: "2475687"
-},
-{
-    name: "Providence",
-    code: "2477058"
-},
-{
-    name: "Raleigh",
-    code: "2478307"
-},
-{
-    name: "Richmond",
-    code: "2480894"
-},
-{
-    name: "Sacramento",
-    code: "2486340"
-},
-{
-    name: "Salt Lake City",
-    code: "2487610"
-},
-{
-    name: "San Antonio",
-    code: "2487796"
-},
-{
-    name: "San Diego",
-    code: "2487889"
-},
-{
-    name: "San Francisco",
-    code: "2487956"
-},
-{
-    name: "San Jose",
-    code: "2488042"
-},
-{
-    name: "Seattle",
-    code: "2490383"
-},
-{
-    name: "St. Louis",
-    code: "2486982"
-},
-{
-    name: "Tallahassee",
-    code: "2503713"
-},
-{
-    name: "Tampa",
-    code: "2503863"
-},
-{
-    name: "Tucson",
-    code: "2508428"
-},
-{
-    name: "Virginia Beach",
-    code: "2512636"
-},
-{
-    name: "Washington",
-    code: "2514815"
-}
+        name: "Alabama",
+        code: "2364559"
+    },
+    {
+        name: "Arizona",
+        code: "2449808"
+    },
+    {
+        name: "California",
+        code: "2442047"
+    },
+    {
+        name: "Colorado",
+        code: "2383489"
+    },
+    {
+        name: "Connecticut",
+        code: "2458410"
+    },
+    {
+        name: "Florida",
+        code: "2428344"
+    },
+    {
+        name: "Georgia",
+        code: "2357024"
+    },
+    {
+        name: "Hawaii",
+        code: "2423945"
+    },
+    {
+        name: "Illinois",
+        code: "2379574"
+    },
+    {
+        name: "Indiana",
+        code: "2427032"
+    },
+    {
+        name: "Louisiana",
+        code: "2359991"
+    },
+    {
+        name: "Kentucky",
+        code: "2442327"
+    },
+    {
+        name: "Maryland",
+        code: "22358820"
+    },
+    {
+        name: "Massachusetts",
+        code: "2367105"
+    },
+    {
+        name: "Michigan",
+        code: "2391585"
+    },
+    {
+        name: "Minnesota",
+        code: "2452078"
+    },
+    {
+        name: "Mississippi",
+        code: "2428184"
+    },
+    {
+        name: "Missouri",
+        code: "2430683"
+    },
+    {
+        name: "Nebraska",
+        code: "2465512"
+    },
+    {
+        name: "New Mexico",
+        code: "2352824"
+    },
+    {
+        name: "New York",
+        code: "2459115"
+    },
+    {
+        name: "Nevada",
+        code: "2436704"
+    },
+    {
+        name: "North Carolina",
+        code: "2378426"
+    },
+    {
+        name: "Ohio",
+        code: "2380358"
+    },
+    {
+        name: "Oklahoma",
+        code: "2464592"
+    },
+    {
+        name: "Oregon",
+        code: "2475687"
+    },
+    {
+        name: "Pennsylvania",
+        code: "2418046"
+    },
+    {
+        name: "Rhode Island",
+        code: "2477058"
+    },
+    {
+        name: "Tennessee",
+        code: "2449323"
+    },
+    {
+        name: "Texas",
+        code: "2357536"
+    },
+    {
+        name: "Utah",
+        code: "2487610"
+    },
+    {
+        name: "Virginia",
+        code: "2460389"
+    },
+    {
+        name: "Washington State",
+        code: "2490383"
+    },
+    {
+        name: "Wisconsin",
+        code: "2451822"
+    }
+// {
+//     name: "Cleveland",
+//     code: "2381475"
+// },
+// {
+//     name: "Columbus",
+//     code: "2383660"
+// },
+// {
+//     name: "Dallas-Ft. Worth",
+//     code: "2388929"
+// },
+// {
+//     name: "Denver",
+//     code: "2391279"
+// },
+// {
+//     name: "Greensboro",
+//     code: "2414469"
+// },
+// {
+//     name: "Houston",
+//     code: "2424766"
+// },
+// {
+//     name: "Long Beach",
+//     code: "2441472"
+// },
+// {
+//     name: "Miami",
+//     code: "2450022"
+// },
+// {
+//     name: "Nashville",
+//     code: "2457170"
+// },
+// {
+//     name: "New Orleans",
+//     code: "2458833"
+// },
+// {
+//     name: "Orlando",
+//     code: "2466256"
+// },
+// {
+//     name: "Philadelphia",
+//     code: "2471217"
+// },
+// {
+//     name: "Phoenix",
+//     code: "2471390"
+// },
+// {
+//     name: "Pittsburgh",
+//     code: "2473224"
+// },
+// {
+//     name: "Raleigh",
+//     code: "2478307"
+// },
+// {
+//     name: "Richmond",
+//     code: "2480894"
+// },
+// {
+//     name: "Sacramento",
+//     code: "2486340"
+// },
+// {
+//     name: "San Antonio",
+//     code: "2487796"
+// },
+// {
+//     name: "San Diego",
+//     code: "2487889"
+// },
+// {
+//     name: "San Francisco",
+//     code: "2487956"
+// },
+// {
+//     name: "San Jose",
+//     code: "2488042"
+// },
+// {
+//     name: "St. Louis",
+//     code: "2486982"
+// },
+// {
+//     name: "Tallahassee",
+//     code: "2503713"
+// },
+// {
+//     name: "Tampa",
+//     code: "2503863"
+// },
+// {
+//     name: "Tucson",
+//     code: "2508428"
+// },
+// {
+//     name: "Virginia Beach",
+//     code: "2512636"
+// },
+// {
+//     name: "Washington",
+//     code: "2514815"
+// }
 ]
 
 // generateTrendingList(myTrend)
